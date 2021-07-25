6252c0fc5aaf2fffc3c6ec8d539d8cf1d3fb47aa
danielmekuriaw <daniel.mekuriaw16@gmail.com>
2021-07-24 20:28:23 -0500

    Task Lister Lab DONE

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+  const form = document.querySelector("#create-task-form")
+
+  form.addEventListener('submit', function(event){
+    event.preventDefault()
+
+    let task = document.querySelector("#tasks")
+    
+    let li = document.createElement("li")
+
+    li.textContent = event.target[0].value
+
+    let deleteBtn = document.createElement("button")
+    deleteBtn.textContent = "X"
+
+    deleteBtn.addEventListener('click', function(event){
+      event.preventDefault()
+      li.remove()
+    })
+
+    li.append(deleteBtn)
+
+    task.append(li)
+
+    form.reset()
+  })
+
+
+
+  
 });
